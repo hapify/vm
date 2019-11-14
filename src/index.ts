46eeb805fbbd07aa0957dabe02fe38253ab1cd13
Edouard Demotes-Mainard <edouard@tractr.net>
2019-11-14 12:50:28 -0500

    Run linter and fix tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import { VM } from 'vm2';
 
 const SECOND = 1000;
@@ -28,7 +27,6 @@ export class IntegrityError extends Error {
 }
 
 export class HapifyVM {
-
 	/** Default options */
 	private defaultOptions: HapifyVMOptions = {
 		timeout: SECOND
@@ -54,13 +52,12 @@ export class HapifyVM {
 
 	/** Execute content */
 	run(content: string, context: { [key: string]: any }): string {
-
 		let result;
 
 		const vm = new VM({
 			timeout: this.options.timeout,
 			sandbox: Object.assign(context, this.forbiddenObjects),
-			compiler: "javascript",
+			compiler: 'javascript',
 			eval: false,
 			wasm: false
 		});
@@ -69,7 +66,6 @@ export class HapifyVM {
 		try {
 			result = vm.run(wrappedContent);
 		} catch (error) {
-
 			// Check error
 			if (typeof error.message !== 'string' || typeof error.stack !== 'string') {
 				throw new IntegrityError('Invalid error');
@@ -93,7 +89,7 @@ export class HapifyVM {
 			throw evalError;
 		}
 
-		if (typeof result !== "undefined" && typeof result !== "string") {
+		if (typeof result !== 'undefined' && typeof result !== 'string') {
 			throw new OutputError('Must return a string');
 		}
 
--- src/tests/usage.ts
+++ src/tests/usage.ts
@@ -1,50 +1,48 @@
-import {expect, fail} from '@hapi/code';
+import { expect, fail } from '@hapi/code';
 import * as Lab from '@hapi/lab';
 const lab = (exports.lab = Lab.script());
 
-import { HapifyVM } from "../index";
+import { HapifyVM } from '../index';
 
 lab.test('normal', async () => {
-    expect(new HapifyVM().run('return new Date().toString();', {})).to.be.a.string();
+	expect(new HapifyVM().run('return new Date().toString();', {})).to.be.a.string();
 });
 lab.test('context access', async () => {
-    expect(new HapifyVM().run('return value', { value: 'TheValue' })).to.equal('TheValue');
-    expect(new HapifyVM().run('return value.prop', { value: { prop: 'TheValue' } })).to.equal('TheValue');
+	expect(new HapifyVM().run('return value', { value: 'TheValue' })).to.equal('TheValue');
+	expect(new HapifyVM().run('return value.prop', { value: { prop: 'TheValue' } })).to.equal('TheValue');
 });
 lab.test('no return', async () => {
-    expect(new HapifyVM().run('return;', {})).to.be.undefined()
+	expect(new HapifyVM().run('return;', {})).to.be.undefined();
 });
 
 lab.test('return non string', async () => {
-    expect(() => new HapifyVM().run('return 1;', {})).to.throw('Must return a string');
+	expect(() => new HapifyVM().run('return 1;', {})).to.throw('Must return a string');
 });
 
 lab.test('timeout', async () => {
-    expect(() => new HapifyVM({ timeout: 200 }).run('while(true) {}', {})).to.throw('Script execution timed out. (200ms)');
+	expect(() => new HapifyVM({ timeout: 200 }).run('while(true) {}', {})).to.throw('Script execution timed out. (200ms)');
 });
 
 lab.test('evaluation error 1', async () => {
-    try {
-        new HapifyVM({ timeout: 200 }).run('/* comment */ a();', {});
-        fail('Should throw an error');
-    } catch (e) {
-        expect(e.message).to.equal('a is not defined');
-        expect(e.details).to.be.a.string();
-        expect(e.lineNumber).to.equal(1);
-        expect(e.columnNumber).to.equal(15);
-    }
-
+	try {
+		new HapifyVM({ timeout: 200 }).run('/* comment */ a();', {});
+		fail('Should throw an error');
+	} catch (e) {
+		expect(e.message).to.equal('a is not defined');
+		expect(e.details).to.be.a.string();
+		expect(e.lineNumber).to.equal(1);
+		expect(e.columnNumber).to.equal(15);
+	}
 });
 
 lab.test('evaluation error 2', async () => {
-    try {
-        new HapifyVM({ timeout: 200 }).run('function() { return 3;', {});
-        fail('Should throw an error');
-    } catch (e) {
-        expect(e.message).to.equal('Unexpected token (');
-        expect(e.details).to.be.a.string();
-        expect(e.lineNumber).to.be.a.number();
-        expect(e.columnNumber).to.be.a.number();
-    }
+	try {
+		new HapifyVM({ timeout: 200 }).run('function() { return 3;', {});
+		fail('Should throw an error');
+	} catch (e) {
+		expect(e.message).to.equal('Unexpected token (');
+		expect(e.details).to.be.a.string();
+		expect(e.lineNumber).to.be.a.number();
+		expect(e.columnNumber).to.be.a.number();
+	}
 });
-
