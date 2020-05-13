13a79aef2ed5f83b6173752387e90101c822d111
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-05-12 23:56:59 -0400

    ci: add codecov config

--- .labrc.js
+++ .labrc.js
@@ -3,6 +3,6 @@ module.exports = {
     'coverage-exclude': 'dist/tests',
     threshold: 95,
     sourcemaps: true,
-    reporter: ['console', 'lcov'],
-    output: ['stdout', 'lcov.info']
+    reporter: ['console', 'html', 'lcov', 'json'],
+    output: ['stdout', 'coverage-report/coverage.html', 'coverage-report/lcov.info', 'coverage-report/data.json']
 };
