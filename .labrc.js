37695eac7d1952d3dfe9a3e3ee6b6c9a1a40680c
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-05-16 10:14:28 -0400

    build: use ts-node, mocha & nyc

--- .labrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    coverage: true,
-    'coverage-exclude': 'dist/tests',
-    threshold: 95,
-    sourcemaps: true,
-    reporter: ['console', 'html', 'lcov', 'json'],
-    output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info', 'coverage/data.json']
-};
--- src/index.ts
+++ src/index.ts
@@ -69,9 +69,7 @@ export class HapifyVM {
 			result = vm.run(wrappedContent);
 		} catch (error) {
 			// Check error
-			/* $lab:coverage:off$ */
 			if (typeof error.message !== 'string' || typeof error.stack !== 'string') {
-				/* $lab:coverage:on$ */
 				throw new IntegrityError('Invalid error');
 			}
 
@@ -82,9 +80,7 @@ export class HapifyVM {
 			// Parse error
 			const evalError = new EvaluationError(error.message);
 			const matches = this.stackRegex.exec(error.stack);
-			/* $lab:coverage:off$ */
 			if (matches) {
-				/* $lab:coverage:on$ */
 				const lineNumber = Number(matches[1]);
 				const columnNumber = Number(matches[2]);
 				evalError.details = `Error: ${evalError.message}. Line: ${lineNumber}, Column: ${columnNumber}`;
